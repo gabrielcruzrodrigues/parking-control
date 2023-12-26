edd16ab52abaea5aad13458c416eba8cebf31c8c
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2023-12-26 12:53:23 -0300

    creating authenticationManagerBuilder

--- src/main/java/com/api/parkingcontrol/security/WebSecurityConfig.java
+++ src/main/java/com/api/parkingcontrol/security/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.api.parkingcontrol.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -15,5 +16,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("gabriel")
+                .password("123456")
+                .roles("ADMIN");
     }
 }
