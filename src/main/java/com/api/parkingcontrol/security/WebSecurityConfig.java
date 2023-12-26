819a9a042918c2703407e2450128baa47205964a
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2023-12-26 12:24:30 -0300

    creating WebSecurityConfig class and starting basic config

--- src/main/java/com/api/parkingcontrol/security/WebSecurityConfig.java
+++ src/main/java/com/api/parkingcontrol/security/WebSecurityConfig.java
@@ -1,7 +1,17 @@
 package com.api.parkingcontrol.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .authorizeHttpRequests()
+                .anyRequest().authenticated();
+    }
 }
